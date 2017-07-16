0b15c5de9edeac686a146ebc45938268adb943e6
Piotr Muzyka <piotrmuzyka@yahoo.com>
2017-07-16 17:42:55 +0200

    Initial commit.

---
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp;
+
+/**
+ * Created by PiotrM on 16.07.2017.
+ */
+
+public class EditorActivity {
+}
---
+++ app/src/main/java/com/example/android/inventoryapp/data/ItemContract.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp.data;
+
+/**
+ * Created by PiotrM on 16.07.2017.
+ */
+
+public class ItemContract {
+}
